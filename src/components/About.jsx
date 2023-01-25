a6c9fb895bd5a4eb5cac7c69c1b4aa52b09c3961
koltvictor <kolts.email@gmail.com>
2023-01-25 15:29:49 -0800

    updating wordage in about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,10 +5,9 @@ export default function About() {
   return (
     <AboutWrapper>
         <h4>WHERE WILL YOUR JOURNEY TAKE YOU?</h4><br/>
-        <p className="centeredText">Are you a discerning traveler seeking to explore and discover the world’s most incredible destinations? Do you dream of adventure on the African plains or a life-changing expedition through the Arctic? Perhaps you are in a stage of your life where you seek peace, tranquility, and a relaxing escape to your private island where azure waters meet pristine white sand beaches. Boutique Travel Advisors is a luxury travel agency based out of Scottsdale, Arizona specializes in designing once-in-a-lifetime experiences for individuals, families, and teams.
-
-        Stop settling for plain uninspired vacations. Instead, discover how bespoke travel experiences explicitly crafted for YOU will transform your travels. Let our luxury Virtuoso travel agency show you the way.
-        </p>     
+        <p className="centeredText">Are you a discerning traveler seeking to explore and discover the world’s most incredible destinations? Do you dream of adventure on the African plains or a life-changing expedition through the Arctic? Perhaps you are in a stage of your life where you seek peace, tranquility, and a relaxing escape to your private island where azure waters meet pristine white sand beaches. Tailored Travel is a luxury travel agency based out of New York City and specializes in designing once-in-a-lifetime experiences for individuals, families, teams, and groups.
+<br/><br/>
+        Stop settling for plain uninspired vacations. Instead, discover how bespoke travel experiences explicitly crafted for your desires will transform your travels.        </p>     
     </AboutWrapper>
   )
 }
