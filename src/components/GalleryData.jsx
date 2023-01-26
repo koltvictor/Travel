f6bd991ded7cd8239afbb759df88ecc0e3d71b4c
koltvictor <kolts.email@gmail.com>
2023-01-26 10:08:41 -0800

    added 20 images to GalleryData

--- src/components/GalleryData.jsx
+++ src/components/GalleryData.jsx
@@ -1,7 +1,66 @@
-import React from 'react'
+export const GalleryData = [
+    {
+        image: 'https://i.imgur.com/ZSL5YTz.png'
+    },
+    {
+        image: 'https://i.imgur.com/2xeW8Qf.png'
+    },
+    {
+        image: 'https://i.imgur.com/rr6wXQk.png'
+    },
+    {
+        image: 'https://i.imgur.com/23C661g.png'
+    },
+    {
+        image: 'https://i.imgur.com/cDZ286V.png'
+    },
+    {
+        image: 'https://i.imgur.com/Qc6wgrG.png'
+    },
+    {
+        image: 'https://i.imgur.com/YbeUTJH.png'
+    },
+    {
+        image: 'https://i.imgur.com/tlarxcu.png'
+    },
+    {
+        image: 'https://i.imgur.com/zzZnwAH.png'
+    },
+    {
+        image: 'https://i.imgur.com/58Ot5Vz.png'
+    },
+    {
+        image: 'https://i.imgur.com/zUzJQGg.png'
+    },
+    {
+        image: 'https://i.imgur.com/K8cpMp6.png'
+    },
+    {
+        image: 'https://i.imgur.com/rkc6Mru.png'
+    },
+    {
+        image: 'https://i.imgur.com/rEiazim.png'
+    },
+    {
+        image: 'https://i.imgur.com/6Ml0cOY.png'
+    },
+    {
+        image: 'https://i.imgur.com/q9N6ooP.png'
+    },
+    {
+        image: 'https://i.imgur.com/URXl2yL.png'
+    },
+    {
+        image: 'https://i.imgur.com/2eAJWoj.png'
+    },
+    {
+        image: 'https://i.imgur.com/fkGcOTR.png'
+    },
+    {
+        image: 'https://i.imgur.com/lGHxFyp.png'
+    },
+    {
+        image: 'https://i.imgur.com/LQuF0D6.png'
+    },
 
-export default function GalleryData() {
-  return (
-    <div>GalleryData</div>
-  )
-}
+]
