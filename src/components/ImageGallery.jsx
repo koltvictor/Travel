a1bd3d20a2557d65623956663274d0903ac633b0
koltvictor <kolts.email@gmail.com>
2023-01-27 08:49:23 -0800

    added react lightgallery and styling for gallery page

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -1,68 +1,27 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { GalleryData } from '../data/GalleryData'
 import styled from 'styled-components';
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Card from 'react-bootstrap/Card'
-import Lightbox from 'react-image-lightbox'
+import { LightgalleryItem } from "react-lightgallery";
 
 export default function ImageGallery() {
-    const [photos, setPhotos] = useState([]);
-    const columnsPerRow = 3;
-
-    const getGalleryData = async () => {
-        const response = GalleryData;
-        setPhotos(response);
-    }
-
-    const getColumnsForRow = () => {
-        let items = photos.map((image) => {
-            return (
-                <Col className="cardGrid">
-                    <Card className="cardImage">
-                        <Card.Img src={image.image} className="galleryImage" />
-                    </Card>
-                </Col>
-            );
-        });
-        return items;
-    };
-
-    useEffect(() => {
-        getGalleryData();
-    })
-
   return (
-    <GalleryWrapper>
-        <Row md={columnsPerRow}>
-            {getColumnsForRow()}
-        </Row>
-
-        {/* {isOpen && (
-      <Lightbox
-        mainSrc={images[photoIndex]}
-        nextSrc={images[(photoIndex + 1) % images.length]}
-        prevSrc={images[(photoIndex + images.length - 1) % images.length]}
-        onCloseRequest={() => this.setState({ isOpen: false })}
-        onMovePrevRequest={() =>
-          this.setState({
-            photoIndex: (photoIndex + images.length - 1) % images.length,
-          })
-        }
-        onMoveNextRequest={() =>
-          this.setState({
-            photoIndex: (photoIndex + 1) % images.length,
-          })
-        }
-      />
-    )} */}
-    
-    </GalleryWrapper>
+    <div className="galleryFullWrap">
+        <h4>Click on Images to Expand</h4>
+        <GalleryWrapper>
+            {GalleryData.map((photos) => {
+                return (
+                    <LightgalleryItem src={photos.image} group="any" subHtml={photos.info}>
+                        <img src={photos.image} alt={photos.info}  className="galleryImage" />
+                    </LightgalleryItem>
+                )
+            })}
+        </GalleryWrapper>
+    </div>
   )
 }
 
 const GalleryWrapper = styled.div`
-display: flex;
-flex-direction: row;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
     padding: 10rem 5rem 0rem 5rem;
 `
--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "lightgallery.js/dist/css/lightgallery.css";
+import { LightgalleryProvider } from "react-lightgallery";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <React.StrictMode>
-      <App />
+      <LightgalleryProvider>
+        <App />
+      </LightgalleryProvider>
     </React.StrictMode>
   </BrowserRouter>
 );
